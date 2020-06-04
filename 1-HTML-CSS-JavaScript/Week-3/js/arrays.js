75e0913b111303abe53a9d990e49d4689cd0638b
th3hungryd3v <th3hungryd3v@gmail.com>
2020-06-04 02:58:48 +0000

    arrays.js

---
+++ 1-HTML-CSS-JavaScript/Week-3/js/arrays.js
@@ -0,0 +1,33 @@
+// What are Arrays?
+// Creating an Array
+// Accessing Array items
+// Modifying Array items
+// Array property: length
+// Array Methods: pop(), push(), shift(), unshift(), join(), includes(), indexOf()
+
+// Arrays are numerically indexed collections of values
+// Arrays can contain strings, numbers, Booleans, even other Arrays
+// Each item in an Array is associated with a number based on its order of appearance in the array, starting with 0
+
+// Creating an Array
+// To create an Array using the Array Literal Syntax:
+// [item1,item2,item3,...] = your objects go inside the square brackets seperated by commas
+// To use an Array more than once, store the Array literal in a variable:
+// const groceryList = ['eggs','coffee beans','salad'];
+// let luckyNumbers = [7,23,99,11,777];
+// You can also mix variables up like so:
+// let anArray = [7,'eggs',99,'salad',true];
+
+// Accessing Array Items
+// Arrays are zero-indexed: an Array's index starts at 0
+// const groceryList = ['eggs','coffee beans','salad'];
+//           Array Item |'eggs'|'coffe beans'|'salad'|         
+//           Array Index|  0   |      1      |   2   |
+
+// Using bracket notation you can access the objects/items in an Array:
+// variableName immediately followed by [] with the index number inside of it
+// groceryList[0] ==> 'eggs'
+// groceryList[1] ==> 'coffee beans'
+// groceryList[2] ==> 'salad'
+
+// Modifying Array Items
