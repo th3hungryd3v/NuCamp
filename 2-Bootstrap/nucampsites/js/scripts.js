2a417fb0f2cd24b0ba8143cb7ee12b0dd24ec31a
th3hungryd3v <th3hungryd3v@gmail.com>
2020-07-04 17:53:59 +0000

    scripts.js

---
+++ 2-Bootstrap/nucampsites/js/scripts.js
@@ -0,0 +1,25 @@
+// JQuery script for the Play/Pause Buttons to work 
+// JQuery Ready Method
+        // Shorthand of $( document).ready(function(){});
+        $(function() {
+            // Sets the carousel to cycle every 2 seconds, Default is 5000 (5 seconds)
+            $(".carousel").carousel( { interval: 2000 } );
+            // Attaches the matching ID to a click event which is #carouselPause
+            $("#carouselButton").click(function(){
+                // if this button is clicked, check the child nodes("i element") to see if it has a class(.hasClass("className") of "fa-pause"
+                if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                    // if it does, then we know the carousel is currently playing, So clicking on pause, will pause the carousel
+                    $(".carousel").carousel("pause");
+                    // when the pause button (#carouselButton) is clicked:
+                    // remove the i element child node that has the class of fa-pause(pause button)
+                    $("#carouselButton").children("i").removeClass("fa-pause");
+                    // And replace (add) it with the same element (i) child node but with a different class(font-awesome play button) to display the play button
+                    $("#carouselButton").children("i").addClass("fa-play");
+                } else {
+                    // otherwise, we know the carousel is currently paused and the play button(fa-play) is displayed and clicking the visible play button will start cycling the carousel again while changing to a pause button
+                    $(".carousel").carousel("cycle");
+                    $("#carouselButton").children("i").removeClass("fa-play");
+                    $("#carouselButton").children("i").addClass("fa-pause")
+                }
+            });
+        });
