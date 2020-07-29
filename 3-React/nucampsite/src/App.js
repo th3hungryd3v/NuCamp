3464f75674a3eb29c56b597ea6b4fea8b7d41526
th3hungryd3v <th3hungryd3v@gmail.com>
2020-07-29 04:21:26 +0000

    React setup

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Yourself React Everybody
         </a>
       </header>
     </div>
