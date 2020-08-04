3bf274d515774b22616ad3ea85a77a54dbf92368
th3hungryd3v <th3hungryd3v@gmail.com>
2020-08-04 05:03:19 +0000

    Init CS

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Yourself React Everybody
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              NuCamp Bo0tCamp
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
+
+export default App;
+
+//import logo from './logo.svg';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn Yourself React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
