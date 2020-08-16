62711cb73ea889401f850bffdaa1eda28b768fa1
th3hungryd3v <th3hungryd3v@gmail.com>
2020-08-16 00:36:04 +0000

    React Router

--- 3-React/nucampsite/src/components/HeaderComponent.js
+++ 3-React/nucampsite/src/components/HeaderComponent.js
@@ -54,7 +54,7 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/home">
+                                    <NavLink className="nav-link" to="/contactus">
                                         <i className="fa fa-address-card fa-lg" /> Contact Us
                                     </NavLink>
                                 </NavItem>
--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -1,11 +1,11 @@
 //~~**##__ IMPORTS __##--**~~\\
 import React, { Component } from "react";
 import Header from './HeaderComponent';
-import Footer from './FooterComponent'
-import Directory from "./DirectoryComponent"
-import Home from './HomeComponent'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { CAMPSITES } from "../shared/campsites"
+import Footer from './FooterComponent';
+import Directory from "./DirectoryComponent";
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component { // Create child class of Component imported from React
   constructor(props) {
