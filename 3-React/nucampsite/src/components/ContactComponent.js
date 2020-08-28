60089d48cf79def86761c3ba14ed3ba4a542d24c
th3hungryd3v <th3hungryd3v@gmail.com>
2020-08-28 09:09:15 -0700

    React Redux Forms Revisited

--- 3-React/nucampsite/src/components/ContactComponent.js
+++ 3-React/nucampsite/src/components/ContactComponent.js
@@ -7,7 +7,7 @@ import {
   Col,
   Row,
 } from "reactstrap";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Control, Form, Errors, actions } from "react-redux-form";
 import { Link } from "react-router-dom";
 
 const required = (val) => val && val.length;
@@ -43,6 +43,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current state is: " + JSON.stringify(values));
     alert("Current state is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
   }
 
   render() {
@@ -92,7 +93,7 @@ class Contact extends Component {
             <hr />
           </div>
           <div className="col-md-10">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form model="feedbackForm" onSubmit={(values) => this.handleSubmit(values)}>
               <Row className="form-group">
                 <Label htmlFor="firstName" md={2}>
                   First Name
@@ -258,7 +259,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
