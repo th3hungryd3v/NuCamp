9f42588bfe301ddacc9513b7e659531493b9a6f9
thehungryd3v <th3hungryd3v@gmail.com>
2020-09-06 15:53:57 -0700

    nucampsites folder added back

---
+++ 4-React-Native/nucampsite/components/MainComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Platform } from 'react-native';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { CAMPSITES } from '../shared/campsites';
+import { createStackNavigator } from 'react-navigation';
+
+console.disableYellowBox = true; // Yellow Warnings DISABLED
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory},
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+class Main extends Component {
+    render() {
+        return ( 
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+                }}>
+                <DirectoryNavigator />
+            </View>
+        )    
+    }
+}
+
+export default Main;
