f8864fbd2343de22053b20703bc7bea801bc78c0
th3hungryd3v <th3hungryd3v@gmail.com>
2020-09-14 15:46:39 -0700

    Prettied up

--- 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
+++ 4-React-Native/nucampsite/components/CampsiteInfoComponent.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
+import React, { Component } from "react";
+import { Text, View, ScrollView, FlatList } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
 
 function RenderCampsite(props) {
-
-    const {campsite} = props;
-    if (campsite) {
-        return (
-            <Card
-                featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}>
-                <Text style={{margin:10}}>
-                    {campsite.description}
-                </Text>
-                <Icon 
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() => props.favorite ? console.log('Already set as a favorite.') : props.markFavorite()}
-                />
-            </Card>
-        );
-    }
-    return <View />
+  const { campsite } = props;
+  if (campsite) {
+    return (
+      <Card
+        featuredTitle={campsite.name}
+        image={require("./images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{campsite.description}</Text>
+        <Icon
+          name={props.favorite ? "heart" : "heart-o"}
+          type="font-awesome"
+          color="#f50"
+          raised
+          reverse
+          onPress={() =>
+            props.favorite
+              ? console.log("Already set as a favorite.")
+              : props.markFavorite()
+          }
+        />
+      </Card>
+    );
+  }
+  return <View />;
 }
 
 function RenderComments({ comments }) {
@@ -52,36 +54,43 @@ function RenderComments({ comments }) {
   );
 }
 
-
 class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            favorite: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      favorite: false,
+    };
+  }
 
-    markFavorite() {
-        this.setState({favorite: true});
-    }
+  markFavorite() {
+    this.setState({ favorite: true });
+  }
 
-    static navigationOptions = {
-        title: 'Campsite Information'
-    }
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
 
-    render() {
-        const campsiteId = this.props.navigation.getParam('campsiteId');
-        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId);
-        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
-        return (
-            <ScrollView>
-                <RenderCampsite campsite={campsite} favorite={this.state.favorite} markFavorite={() => this.markFavorite()} />
-                <RenderComments comments={comments} />
-            </ScrollView>
-        );
-    }
+  render() {
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    const campsite = this.state.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    );
+    const comments = this.state.comments.filter(
+      (comment) => comment.campsiteId === campsiteId
+    );
+    return (
+      <ScrollView>
+        <RenderCampsite
+          campsite={campsite}
+          favorite={this.state.favorite}
+          markFavorite={() => this.markFavorite()}
+        />
+        <RenderComments comments={comments} />
+      </ScrollView>
+    );
+  }
 }
 
 export default CampsiteInfo;
--- 4-React-Native/nucampsite/components/HomeComponent.js
+++ 4-React-Native/nucampsite/components/HomeComponent.js
@@ -1,51 +1,53 @@
-import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
 
-function RenderItem({item}) {
-    if (item) {
-        return (
-            <Card
-                featuredTitle={item.name}
-                image={require('./images/react-lake.jpg')}>
-                <Text
-                    style={{margin: 10}}>
-                    {item.description}
-                </Text>
-            </Card>
-        )
-    }
-    return <View />
+function RenderItem({ item }) {
+  if (item) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        image={require("./images/react-lake.jpg")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
 }
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      partners: PARTNERS,
+      promotions: PROMOTIONS,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        }
-    }
+  static navigationOptions = {
+    title: "Home",
+  };
 
-    static navigationOptions = {
-        title: 'Home'
-    }
-
-    render() {
-        return (
-            <ScrollView>
-                <RenderItem
-                    item={this.state.campsites.filter(campsite => campsite.featured)[0]} />
-                <RenderItem
-                    item={this.state.promotions.filter(promotion => promotion.featured)[0]} />
-                <RenderItem
-                    item={this.state.partners.filter(partner => partner.featured)[0]} />
-            </ScrollView>
-        );
-    }
+  render() {
+    return (
+      <ScrollView>
+        <RenderItem
+          item={this.state.campsites.filter((campsite) => campsite.featured)[0]}
+        />
+        <RenderItem
+          item={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+        />
+        <RenderItem
+          item={this.state.partners.filter((partner) => partner.featured)[0]}
+        />
+      </ScrollView>
+    );
+  }
 }
 export default Home;
