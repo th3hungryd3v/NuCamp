94337e0260c6bfb4ce2632ca2405f8b7b134f797
th3hungryd3v <th3hungryd3v@penguin>
2020-09-18 03:00:26 -0700

    baseUrl.js

--- 4-React-Native/nucampsite/shared/baseUrl.js
+++ 4-React-Native/nucampsite/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://100.115.92.205:3001/'; //  Ternary Operator ==> ? 'http://0.0.0.0:3001' : console.error('Network Error')
+export const baseUrl = 'http://192.168.10.64:3001/'; //  Ternary Operator ==> ? 'http://0.0.0.0:3001' : console.error('Network Error')
