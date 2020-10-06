6943cde63e1bf3bb1cf06fb643b050982b69e737
thehungryd3v <th3hungryd3v@gmail.com>
2020-10-05 21:57:55 -0700

    fixed showNetInfo

--- 4-React-Native/nucampsite/components/MainComponent.js
+++ 4-React-Native/nucampsite/components/MainComponent.js
@@ -329,23 +329,32 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPartners();
     this.props.fetchPromotions();
-
-    NetInfo.fetch().then((connectionInfo) => {
-      Platform.OS === 'ios'
-        ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-        : ToastAndroid.show(
-            'Initial Network Connectivity Type: ' + connectionInfo.type,
-            ToastAndroid.LONG
-          );
-    });
-
+    
+    
     this.unsubscribeNetInfo = NetInfo.addEventListener((connectionInfo) => {
       this.handleConnectivityChange(connectionInfo);
     });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeNetInfo();
+    
+    
+    showNetInfo = async () => {
+      const connectionInfo = await NetInfo.fetch();
+      Platform.OS === 'ios'
+      ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+      : ToastAndroid.show(
+        'Initial Network Connectivity Type: ' + connectionInfo.type,
+        ToastAndroid.LONG
+        );
+      };
+      
+      // NetInfo.fetch().then((connectionInfo) => {
+        
+        // });
+        
+      }
+      
+      componentWillUnmount() {
+        this.unsubscribeNetInfo();
+        this.showNetInfo();
   }
 
   handleConnectivityChange = (connectionInfo) => {
