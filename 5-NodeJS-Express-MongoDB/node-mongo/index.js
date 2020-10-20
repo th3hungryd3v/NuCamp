8221682a06d9ffd97efbc68cd991cd5c004e9130
thehungryd3v <th3hungryd3v@gmail.com>
2020-10-19 21:51:09 -0700

    Node MongoDB Example 1

---
+++ 5-NodeJS-Express-MongoDB/node-mongo/index.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient; // This will act as a client for the mongodb server
+const assert = require('assert').strict;
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'nucampsite';
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  assert.strictEqual(err, null);
+
+  console.log('Connected correctly to server');
+
+  const db = client.db(dbname);
+
+  db.dropCollection('campsites', (err, result) => {
+    assert.strictEqual(err, null);
+
+    console.log('Dropped Collection', result);
+
+    const collection = db.collection('campsites');
+
+    collection.insertOne(
+      { name: 'Breadcrumb Trail Campground', description: 'Test' },
+      (err, result) => {
+        assert.strictEqual(err, null);
+        console.log('Insert Document:', result.ops);
+
+        collection.find().toArray((err, docs) => {
+          assert.strictEqual(err, null);
+          console.log('Found Documents:', docs);
+
+          client.close();
+        });
+      }
+    );
+  });
+}); // Prevents deprecation warnings from showing up in our app.
