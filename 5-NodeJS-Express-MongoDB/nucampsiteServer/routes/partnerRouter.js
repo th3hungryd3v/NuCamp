ac73a3f4a8f72da41d4c46c1dd2d39c3da6612d3
thehungryd3v <th3hungryd3v@gmail.com>
2020-10-24 12:33:29 -0700

    Week_2_Done

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Partner = require('../models/partner')
 
 const partnerRouter = express.Router();
 
@@ -7,36 +8,63 @@ partnerRouter.use(bodyParser.json());
 
 partnerRouter
   .route('/')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Partner.find()
+      .then(partners => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+      })
+      .catch(err => next(err));
+    // res.end('Will send all the partners to you');
   })
-  .get((req, res) => {
-    res.end('Will send all the partners to you');
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Partner.create(req.body)
+      .then(partner => {
+        console.log('Partner Created ', partner);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+      })
+      .catch(err => next(err));
+    // res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
   })
   .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /partners');
   })
   .delete((req, res) => {
-    res.end('Deleting all partners');
+    Partner.deleteMany()
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch(err => next(err));
+    // res.end('Deleting all partners');
   });
 
 partnerRouter
   .route('/:partnerId')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
-  .get((req, res) => {
-    res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      // res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
+      .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+      })
+      .catch(err => next(err));
   })
 
   .post((req, res) => {
@@ -46,14 +74,251 @@ partnerRouter
     );
   })
 
-  .put((req, res) => {
-    res.write(`Updating the partner: ${req.params.partnerId}\n`);
-    res.end(`Will update the partner: ${req.body.name}
-        with description: ${req.body.description}`);
+  .put((req, res, next) => {
+    Partner.findByIdAndUpdate(
+      req.params.partnerId,
+      {
+        $set: req.body
+      },
+      { new: true }
+    )
+      .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+      })
+      .catch(err => next(err));
+    // res.write(`Updating the partner: ${req.params.partnerId}\n`);
+    // res.end(`Will update the partner: ${req.body.name}
+    //     with description: ${req.body.description}`);
   })
 
-  .delete((req, res) => {
-    res.end(`Deleting partner: ${req.params.partnerId}`);
+  .delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch(err => next(err));
+    // res.end(`Deleting partner: ${req.params.partnerId}`);
   });
 
+  partnerRouter
+  .route('/:partnerId/comments')
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then(partner => {
+        if (partner) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(partners);
+
+        } else {
+          err = new Error(`Partner ${req.params.partnerId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+    // res.end('Will send all the partners to you');
+  })
+  .post((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then(partner => {
+        if (partner) {
+          partner.comments.push(req.body);
+          partner.save()
+          .then(partner => {
+            // console.log('Partner Created ', partner);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+
+          })
+          .catch((err) => next(err));
+        } else {
+          err = new Error(`Partner ${req.params.partnerId} not found`);
+          err.status = 404;
+          return next(err)
+        }
+      })
+      .catch(err => next(err));
+    // res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+  })
+
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+  })
+  .delete((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+      .then(partner => {
+        if (partner) {
+          for (let i = (partner.comments.length-1); i >= 0; i--) {
+              partner.comments.id(partner.comments[i]._id).remove();
+          }
+          partner.save()
+          .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+
+          })
+          .catch(err => next(err));
+        } else {
+          err = new Error(`Partner ${req.params.partnerId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+    // res.end('Deleting all partners');
+  });
+
+  partnerRouter.route('/:partnerId/comments/:commentId')
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+        if (partner && partner.comments.id(req.params.commentId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner.comments.id(req.params.commentId));
+        } else if (!partner) {
+            err = new Error(`Partner ${req.params.partnerId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}/comments/${req.params.commentId}`);
+})
+.put((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+        if (partner && partner.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                partner.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                partner.comments.id(req.params.commentId).text = req.body.text;
+            }
+            partner.save()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+        } else if (!partner) {
+            err = new Error(`Partner ${req.params.partnerId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+        if (partner && partner.comments.id(req.params.commentId)) {
+            partner.comments.id(req.params.commentId).remove();
+            partner.save()
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+        } else if (!partner) {
+            err = new Error(`Partner ${req.params.partnerId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
 module.exports = partnerRouter;
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const Partner = require('../models/partner')
+
+// const partnerRouter = express.Router();
+
+// partnerRouter.use(bodyParser.json());
+
+// partnerRouter
+//   .route('/')
+//   .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+//   })
+//   .get((req, res) => {
+//     res.end('Will send all the partners to you');
+//   })
+//   .post((req, res) => {
+//     res.end(
+//       `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+//     );
+//   })
+//   .put((req, res) => {
+//     res.statusCode = 403;
+//     res.end('PUT operation not supported on /partners');
+//   })
+//   .delete((req, res) => {
+//     res.end('Deleting all partners');
+//   });
+
+// partnerRouter
+//   .route('/:partnerId')
+//   .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+//   })
+//   .get((req, res) => {
+//     res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
+//   })
+
+//   .post((req, res) => {
+//     res.statusCode = 403;
+//     res.end(
+//       `POST operation not supported on /partners/${req.params.partnerId}`
+//     );
+//   })
+
+//   .put((req, res) => {
+//     res.write(`Updating the partner: ${req.params.partnerId}\n`);
+//     res.end(`Will update the partner: ${req.body.name}
+//         with description: ${req.body.description}`);
+//   })
+
+//   .delete((req, res) => {
+//     res.end(`Deleting partner: ${req.params.partnerId}`);
+//   });
+
+// module.exports = partnerRouter;
--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotionRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/promotionRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Promotion = require('../models/promotion')
 
 const promotionRouter = express.Router();
 
@@ -7,38 +8,63 @@ promotionRouter.use(bodyParser.json());
 
 promotionRouter
   .route('/')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Promotion.find()
+      .then(promotions => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions);
+      })
+      .catch((err) => next(err));
+    // res.end('Will send all the promotions to you');
   })
-  .get((req, res) => {
-    res.end('Will send all the promotions to you');
-  })
-  .post((req, res) => {
-    res.end(
-      `Will add the promotion: ${req.body.name} with description: ${req.body.description}`
-    );
+  .post((req, res, next) => {
+    Promotion.create(req.body)
+      .then(promotion => {
+        console.log('Promotion Created ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+      })
+      .catch(err => next(err));
+    // res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
   })
   .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
   })
   .delete((req, res) => {
-    res.end('Deleting all promotions');
+    Promotion.deleteMany()
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch((err) => next(err));
+    // res.end('Deleting all promotions');
   });
 
 promotionRouter
   .route('/:promotionId')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
-  .get((req, res) => {
-    res.end(
-      `Will send details of the promotion: ${req.params.promotionId} to you`
-    );
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+      // res.end(`Will send details of the promotion: ${req.params.promotionId} to you`);
+      .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+      })
+      .catch(err => next(err));
   })
 
   .post((req, res) => {
@@ -48,14 +74,252 @@ promotionRouter
     );
   })
 
-  .put((req, res) => {
-    res.write(`Updating the promotion: ${req.params.promotionId}\n`);
-    res.end(`Will update the promotion: ${req.body.name}
-        with description: ${req.body.description}`);
+  .put((req, res, next) => {
+    Promotion.findByIdAndUpdate(
+      req.params.promotionId,
+      {
+        $set: req.body
+      },
+      { new: true }
+    )
+      .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+      })
+      .catch(err => next(err));
+    // res.write(`Updating the promotion: ${req.params.promotionId}\n`);
+    // res.end(`Will update the promotion: ${req.body.name}
+    //     with description: ${req.body.description}`);
   })
 
-  .delete((req, res) => {
-    res.end(`Deleting promotion: ${req.params.promotionId}`);
+  .delete((req, res, next) => {
+    Promotion.findByIdAndDelete(req.params.promotionId)
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch(err => next(err));
+    // res.end(`Deleting promotion: ${req.params.promotionId}`);
   });
 
+  promotionRouter
+  .route('/:promotionId/comments')
+  // .all((req, res, next) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   next();
+  // })
+  .get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+      .then(promotion => {
+        if (promotion) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(promotions);
+
+        } else {
+          err = new Error(`Promotion ${req.params.promotionId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+    // res.end('Will send all the promotions to you');
+  })
+  .post((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+      .then(promotion => {
+        if (promotion) {
+          promotion.comments.push(req.body);
+          promotion.save()
+          .then(promotion => {
+            // console.log('Promotion Created ', promotion);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+
+          })
+          .catch(err => next(err));
+        } else {
+          err = new Error(`Promotion ${req.params.promotionId} not found`);
+          err.status = 404;
+          return next(err)
+        }
+      })
+      .catch(err => next(err));
+    // res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
+  })
+
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+  })
+  .delete((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+      .then(promotion => {
+        if (promotion) {
+          for (let i = (promotion.comments.length-1); i >= 0; i--) {
+              promotion.comments.id(promotion.comments[i]._id).remove();
+          }
+          promotion.save()
+          .then(promotion => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+
+          })
+          .catch(err => next(err));
+        } else {
+          err = new Error(`Promotion ${req.params.promotionId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+    // res.end('Deleting all promotions');
+  });
+
+  promotionRouter.route('/:promotionId/comments/:commentId')
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => {
+        if (promotion && promotion.comments.id(req.params.commentId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion.comments.id(req.params.commentId));
+        } else if (!promotion) {
+            err = new Error(`Promotion ${req.params.promotionId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /promotions/${req.params.promotionId}/comments/${req.params.commentId}`);
+})
+.put((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => {
+        if (promotion && promotion.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                promotion.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                promotion.comments.id(req.params.commentId).text = req.body.text;
+            }
+            promotion.save()
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+        } else if (!promotion) {
+            err = new Error(`Promotion ${req.params.promotionId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Promotion.findById(req.params.promotionId)
+    .then(promotion => {
+        if (promotion && promotion.comments.id(req.params.commentId)) {
+            promotion.comments.id(req.params.commentId).remove();
+            promotion.save()
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+        } else if (!promotion) {
+            err = new Error(`Promotion ${req.params.promotionId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
 module.exports = promotionRouter;
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const promotionRouter = express.Router();
+
+// promotionRouter.use(bodyParser.json());
+
+// promotionRouter
+//   .route('/')
+//   .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+//   })
+//   .get((req, res) => {
+//     res.end('Will send all the promotions to you');
+//   })
+//   .post((req, res) => {
+//     res.end(
+//       `Will add the promotion: ${req.body.name} with description: ${req.body.description}`
+//     );
+//   })
+//   .put((req, res) => {
+//     res.statusCode = 403;
+//     res.end('PUT operation not supported on /promotions');
+//   })
+//   .delete((req, res) => {
+//     res.end('Deleting all promotions');
+//   });
+
+// promotionRouter
+//   .route('/:promotionId')
+//   .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+//   })
+//   .get((req, res) => {
+//     res.end(
+//       `Will send details of the promotion: ${req.params.promotionId} to you`
+//     );
+//   })
+
+//   .post((req, res) => {
+//     res.statusCode = 403;
+//     res.end(
+//       `POST operation not supported on /promotions/${req.params.promotionId}`
+//     );
+//   })
+
+//   .put((req, res) => {
+//     res.write(`Updating the promotion: ${req.params.promotionId}\n`);
+//     res.end(`Will update the promotion: ${req.body.name}
+//         with description: ${req.body.description}`);
+//   })
+
+//   .delete((req, res) => {
+//     res.end(`Deleting promotion: ${req.params.promotionId}`);
+//   });
+
+// module.exports = promotionRouter;
