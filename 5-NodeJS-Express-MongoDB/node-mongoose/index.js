966dcca70dcb9b27a9430453ad21a4381bd59964
th3hungryd3v <th3hungryd3v@gmail.com>
2020-10-24 01:15:49 -0700

    Mongoose Part 3

--- 5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -1,39 +1,55 @@
 const mongoose = require('mongoose');
 const Campsite = require('./models/campsite');
 
+
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 connect.then(() => {
-    console.log('Connected correctly to server');
 
-    // const newCampsite = new 
-    Campsite.create({
-        name: 'React Lake Campground',
-        description: 'test'
-    })
+  console.log('Connected correctly to server');
 
-    // newCampsite.save()
-    .then(campsite => {
-        console.log(campsite);
-        return Campsite.find();
-    })
-    .then(campsites => {
-        console.log(campsites);
-        return Campsite.deleteMany();
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch(err => {
-        console.log(err);
-        mongoose.connection.close();
-    })
-})
+  Campsite.create({
+      name: 'React Lake Campground',
+      description: 'test'
+  })
+  .then(campsite => {
+      console.log(campsite);
+
+      return Campsite.findByIdAndUpdate(campsite._id, {
+          $set: { description: 'Updated Test Document' }
+      }, {
+          new: true
+      });
+  })
+  .then(campsite => {
+      console.log(campsite);
+
+      campsite.comments.push({
+          rating: 5,
+          text: 'What a magnificent view!',
+          author: 'Tinus Lorvaldes'
+      });
+
+      return campsite.save();
+  })
+  .then(campsite => {
+      console.log(campsite);
+      return Campsite.deleteMany();
+  })
+  .then(() => {
+      return mongoose.connection.close();
+  })
+  .catch(err => {
+      console.log(err);
+      mongoose.connection.close();
+  });
+});
 
 // const mongoose = require('mongoose');
 // const Campsite = require('./models/campsite');
--- 5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
 // SCHEMA
 const campsiteSchema = new Schema({
     name: {
@@ -11,7 +29,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
@@ -20,3 +39,26 @@ const campsiteSchema = new Schema({
 const Campsite = mongoose.model('Campsite', campsiteSchema) // Instatiate documents
 
 module.exports = Campsite;
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// // SCHEMA
+// const campsiteSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     }
+// }, {
+//     timestamps: true
+// });
+
+// // MODEL
+// const Campsite = mongoose.model('Campsite', campsiteSchema) // Instatiate documents
+
+// module.exports = Campsite;
