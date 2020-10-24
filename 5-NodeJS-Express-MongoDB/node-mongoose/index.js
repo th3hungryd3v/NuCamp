f8172858486600fa940904d6cd729102c73e5463
th3hungryd3v <th3hungryd3v@gmail.com>
2020-10-23 23:35:08 -0700

    Mongoose Part 2

--- 5-NodeJS-Express-MongoDB/node-mongoose/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -11,12 +11,13 @@ const connect = mongoose.connect(url, {
 connect.then(() => {
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    // const newCampsite = new 
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
+    })
 
-    newCampsite.save()
+    // newCampsite.save()
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
@@ -33,3 +34,39 @@ connect.then(() => {
         mongoose.connection.close();
     })
 })
+
+// const mongoose = require('mongoose');
+// const Campsite = require('./models/campsite');
+
+// const url = 'mongodb://localhost:27017/nucampsite';
+// const connect = mongoose.connect(url, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// connect.then(() => {
+//     console.log('Connected correctly to server');
+
+//     const newCampsite = new Campsite({
+//         name: 'React Lake Campground',
+//         description: 'test'
+//     });
+
+//     newCampsite.save()
+//     .then(campsite => {
+//         console.log(campsite);
+//         return Campsite.find();
+//     })
+//     .then(campsites => {
+//         console.log(campsites);
+//         return Campsite.deleteMany();
+//     })
+//     .then(() => {
+//         return mongoose.connection.close();
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         mongoose.connection.close();
+//     })
+// })
