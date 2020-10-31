62cc2cb42e4bddb68cc62dff5d392207c1a73378
th3hungryd3v <th3hungryd3v@gmail.com>
2020-10-31 09:24:20 -0700

    Passport incomplete

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const passport = require('passport');
 const User = require('../models/user');
 const router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
