9367fd3741c1aa5329e2f3a196538c8f8d5fdcd6
thehungryd3v <th3hungryd3v@gmail.com>
2020-11-05 17:39:26 -0800

    Passport Facebook

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/config.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/nucampsite'
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+      clientId: '386402402720730',
+      clientSecret: '1d41b101f7d5a9840dc2ea3ec1a979b9'
+  }
 };
